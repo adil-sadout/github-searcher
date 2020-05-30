f682d2962345eac2688c0803fcd97c4a02b6a9c9
Adil Sadout <adelsadout@hotmail.com>
2020-05-30 10:10:19 +0100

    more buttons takes you to specific of the github account

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import './App.css';
 import axios from 'axios';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
 class App extends Component {
 
   state = {
     users:[],
-    loading:false
+    user:{},
+    loading:false,
+    alert:null
   };
-/*
-  async componentDidMount(){
-    this.setState({loading:true});
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users:res.data , loading:false});
-    console.log(res.data);
-  }
-*/
-  searchUsers= async (text)=>{
+
+  searchUsers = async (text)=>{
 
     this.setState({loading:true});
 
@@ -29,25 +28,73 @@ class App extends Component {
 
   }
 
+  getUser = async (username) =>{
+    this.setState({loading:true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({ user: res.data, loading:false});
+  }
+
+
+
+
+
+
+
+
+
   clearUsers = () =>{
     this.setState({ users:[] , loading : false });
   }
 
+
+  setAlert = (msg,type)=>{
+    this.setState({alert: { msg , type} } );
+    setTimeout(()=>this.setState({alert:null}), 5000)
+  }
+
+
+
+
   render(){
 
-    const { users,loading} = this.state;
+    const { users,user,loading} = this.state;
     return (
-      <div className="App">
-        <Navbar/>
-        <div className="container">
-          <Search 
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-          />
-          <Users loading={loading} users={users}/>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+
+            <Switch>
+                <Route exact path='/' render={props =>(
+                  <Fragment>
+                    <Search 
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users}/>
+                  </Fragment>
+                )} />
+
+                <Route exact path='/about' component={About}/>
+
+
+                <Route exact path='/user/:login' render={props =>(
+                    <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+                    )}
+                />
+
+            </Switch>
+
+
+            
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
   
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import {Link} from 'react-router-dom';
 function UserItem ({user: {login,html_url,avatar_url}}){
 
         return(
@@ -10,7 +10,7 @@ function UserItem ({user: {login,html_url,avatar_url}}){
                     {login}
                 </h3>
                 <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                    <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link>
                 </div>
 
             </div>
